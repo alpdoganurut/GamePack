d925337e2a56914d3394e3e3d8f1bacf166d6170
Alp Dogan Urut <alpdoganurut@gmail.com>
2021-06-11 10:45:37 +0300

    Updated extensions.

--- Runtime/Utilities/Extensions.cs
+++ Runtime/Utilities/Extensions.cs
@@ -1,13 +1,45 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace GamePack.UnityUtilities.Vendor
 {
     public static class Extensions
     {
+        // Transform
         public static void SetGlobalScale (this Transform transform, Vector3 globalScale)
         {
             transform.localScale = Vector3.one;
             transform.localScale = new Vector3 (globalScale.x/transform.lossyScale.x, globalScale.y/transform.lossyScale.y, globalScale.z/transform.lossyScale.z);
+        }
+        
+        // Range (Vector2)
+        public static float GetRandomValueAsRange(this Vector2 v2)
+        {
+            return Random.Range(v2.x, v2.y);
+        }
+        
+        // Collection
+        public static int GetIndexWithMaxValue<T>(this IEnumerable<T> coll, Func<T, float> func)
+        {
+            var smallestVal = float.MinValue;
+            var smallestIndex = 0;
+            var i = 0;
+            foreach (var item in coll)
+            {
+                var val = func(item);
+                
+                if (val > smallestVal)
+                {
+                    smallestIndex = i;
+                    smallestVal = val;
+                }
+
+                i++;
+            }
+
+            return smallestIndex;
         }
     }
 }
