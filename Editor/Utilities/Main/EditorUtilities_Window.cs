c4f2c5c4dab3b50cf03c856796b2fdd10be78676
Alp Dogan Urut <alpdoganurut@gmail.com>
2022-04-19 16:53:26 +0300

    Seperated Rename in EditorUtilities.

--- Editor/Utilities/Main/EditorUtilities_Window.cs
+++ Editor/Utilities/Main/EditorUtilities_Window.cs
@@ -19,64 +19,7 @@ namespace GamePack.Editor.Utilities
         {
             GetWindow<EditorUtilities>();
         }
-
-        #region Rename
-
-        private const string GenericGameObjectName = "GameObject";
-
-        [SerializeField, TabGroup("Rename")] private string _Separator = "-";
-        [SerializeField, TabGroup("Rename")] private bool _ForceRename;
-
-        [Button, TabGroup("Rename")]
-        private void RenameAll()
-        {
-            foreach (var gObject in FindObjectsOfType<GameObject>())
-            {
-                RenameGameObject(gObject);
-            }
-        }
-
-        private void RenameGameObject(GameObject gObject)
-        {
-            var components = gObject.GetComponents<MonoBehaviour>().Where(behaviour =>
-            {
-                // Filter components with UnityEngine namespace
-                var type = behaviour.GetType();
-                var baseNameSpace = type.Namespace?.Split('.')[0];
-
-                return baseNameSpace == null || baseNameSpace != "UnityEngine";
-            }).ToArray();
-
-            // Return if no component exists
-            if (!components.Any()) return;
-
-            Undo.RecordObjects(components, "BaseEditorUtilities/Rename ");
-
-            var suffix = "";
-            foreach (var component in components)
-            {
-                if (suffix != "") suffix += ", ";
-                suffix += $"{component.GetType().Name}";
-            }
-
-            // Check if name already contains suffix
-            if (!_ForceRename && gObject.name.Length >= suffix.Length &&
-                gObject.name.Substring(gObject.name.Length - suffix.Length, suffix.Length) == suffix) return;
-
-            var oldName = gObject.name;
-
-            // gObject.name = gObject.name.TrimEnd();
-            gObject.name = gObject.name.Trim();
-            if (gObject.name == GenericGameObjectName) gObject.name = "";
-
-            // Don't use separator if name is empty
-            var separator = string.IsNullOrWhiteSpace(gObject.name) ? "" : $" {_Separator} ";
-            gObject.name += separator + suffix;
-            Debug.Log($"Renamed {oldName} to {gObject.name}!");
-        }
-
-        #endregion
-
+        
         #region Replace
 
         [Button, TabGroup("Replace")]
