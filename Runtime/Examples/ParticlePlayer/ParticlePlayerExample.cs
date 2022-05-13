2e49f2b3aa891c18f50b44022f799f69f710ab08
Alp Dogan Urut <alpdoganurut@gmail.com>
2022-05-13 15:31:47 +0300

    Minor refactoring.

--- Runtime/Examples/ParticlePlayer/ParticlePlayerExample.cs
+++ Runtime/Examples/ParticlePlayer/ParticlePlayerExample.cs
@@ -1,20 +1,21 @@
+using GamePack.ParticlePlayerSystem;
 using GamePack.Utilities;
 using Sirenix.OdinInspector;
 using UnityEngine;
 
-namespace GamePack.Examples.ParticlePlayer
+namespace GamePack.Examples
 {
     public class ParticlePlayerExample : MonoBehaviour
     {
         [SerializeField, Required] private string[] _Ids;
 
-        private readonly int _hashedFirst = global::GamePack.ParticlePlayerSystem.ParticlePlayer.HashId("first");
+        private readonly int _hashedFirst = ParticlePlayer.HashId("first");
         
         [Button, HideInEditorMode]
         private void SpawnParticle()
         {
-            global::GamePack.ParticlePlayerSystem.ParticlePlayer.Play(_Ids.GetRandom(), Random.insideUnitSphere * 5);
-            global::GamePack.ParticlePlayerSystem.ParticlePlayer.Play(_hashedFirst, Random.insideUnitSphere * 5);
+            ParticlePlayer.Play(_Ids.GetRandom(), Random.insideUnitSphere * 5);
+            ParticlePlayer.Play(_hashedFirst, Random.insideUnitSphere * 5);
         }
     }
 }
